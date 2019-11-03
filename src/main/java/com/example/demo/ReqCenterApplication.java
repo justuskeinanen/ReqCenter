0043ef5223e395761a526bd1718fc7a30fda97a7
Justus Keinänen <justuskeinanen@gmail.com>
2019-11-03 16:03:37 +0200

    First commit

---
+++ src/main/java/com/example/demo/ReqCenterApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReqCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReqCenterApplication.class, args);
+	}
+
+}
